5558a9f0457f88a366172d6e8efa4ff6ce5583ff
swimfinn <swimfinn81@gmail.com>
2023-02-11 14:34:29 -0800

    added search bar

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer"><small>Coded by Eileen Finn</small></footer>
+      </div>
+    </div >
+  );
+}
+
+export default App;
