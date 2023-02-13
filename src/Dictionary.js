ab26f52796582bdad4b4764a5af0fb93dd99dbb7
swimfinn <swimfinn81@gmail.com>
2023-02-12 17:25:06 -0800

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
     function search(event) {
         event.preventDefault();
-        alert(`Searching for definition of "${keyword}"`);
+
+        let apiKey = `09604979-6b69-4bfc-9a27-677673bdd2ab`;
+        let apiUrl = `https://www.dictionaryapi.com/api/v3/references/learners/json/apple?key=${apiKey}/${keyword}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event) {
